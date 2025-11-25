d9795fc838ab1276b2832fe58c6853387bd280de
sjain02 <sunny.jain@walmart.com>
2025-11-25 21:05:08 +0530

    lfu class

--- src/com/sunny/pattern/deisgn/LFUCache.java
+++ src/com/sunny/pattern/deisgn/LFUCache.java
@@ -6,25 +6,25 @@ class LFUCache {
 
     public static void main(String[] args) {
         LFUCache cache = new LFUCache(2);
-        cache.put(1, 1);
-        cache.put(2, 2);
-        System.out.println(cache.get(1));
-        cache.put(3, 3);
-        System.out.println( cache.get(2));
-        System.out.println( cache.get(3));
-        cache.put(4, 4);
-        System.out.println( cache.get(1));
+        cache.put(1, 1); //1==1
+        cache.put(2, 2); // 1=1,2=2
+        System.out.println("first get: "+cache.get(1));
+        cache.put(3, 3);// 2=2,3=3
+        System.out.println("second get: "+ cache.get(2));
+        System.out.println("third get: "+ cache.get(3));
+        cache.put(4, 4); // 3=3,4=4
+        System.out.println("fourth get: "+ cache.get(1));
         System.out.println( cache.get(3));
         System.out.println(cache.get(4));
     }
-    int capacity =2;
+    int capacity =2 ;
     Map<Integer,Integer> keytoval ;
     Map<Integer,Integer> keytofreq;
     Map<Integer, LinkedHashSet<Integer>> freqtokey;
     int minfreq;
 
     public LFUCache(int capacity) {
-           // capacity = this.capacity;
+            capacity = 2;
             keytoval = new HashMap<>();
             keytofreq = new HashMap<>();
             freqtokey = new HashMap<>();
@@ -34,14 +34,12 @@ class LFUCache {
 
     public int get(int key) {
        if(!keytoval.containsKey(key)) return -1;
-       int freq = keytofreq.get(key);
-        if(freq == minfreq && freqtokey.get(key).isEmpty()) {
+      final int freq = keytofreq.get(key);
+        if(freq == minfreq && freqtokey.get(freq).isEmpty()) {
             freqtokey.remove(freq);
             minfreq++;
         }
-        keytofreq.put(key,keytofreq.getOrDefault(key,0)+1);
-        freqtokey.get(freq).remove(key);
-        updateFreq(1+freq,key);
+        putFreq(1+freq,key);
 
 
         return  keytoval.get(key);
@@ -51,8 +49,8 @@ class LFUCache {
         LinkedHashSet<Integer> set = freqtokey.get(freq);
         if(set == null){
             set = new LinkedHashSet<>();
-            set.add(key);
         }
+        set.add(key);
         freqtokey.put(freq,set);
     }
 
@@ -72,7 +70,10 @@ class LFUCache {
 
     public void evictLFUKey(){
          LinkedHashSet<Integer> set  = freqtokey.get(minfreq);
-         if(set == null) return;
+       //  if(set == null){
+             System.out.println("printset:" +set);
+       //      return;
+       //  }
          int key = set.iterator().next();
          set.remove(key);
          keytofreq.remove(key);
